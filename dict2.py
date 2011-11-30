c5af3b6a5868b5bdc0b3984880e0d98987c7ea27
Nick Semenkovich <semenko@alum.mit.edu>
2011-11-30 14:34:20 -0600

    Updates.

---
+++ dict2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from time import sleep
+
+import xml.etree.ElementTree as etree
+
+# From http://stackoverflow.com/questions/127606/editing-xml-as-a-dictionary-in-python
+
+tree = etree.parse('in.xml')
+root = tree.getroot()
+
+def xml_to_dict(el):
+  d={}
+  if el.text:
+    d[el.tag] = el.text
+  else:
+    d[el.tag] = {}
+  children = el.getchildren()
+  if children:
+    d[el.tag] = map(xml_to_dict, children)
+  return d
+
+a = xml_to_dict(root)
+
+mzXML = a['{http://sashimi.sourceforge.net/schema_revision/mzXML_3.2}mzXML'][0]['{http://sashimi.sourceforge.net/schema_revision/mzXML_3.2}msRun']
+print len(mzXML)
+print mzXML[0]
+print mzXML[0].keys()
+
+
+for element in mzXML:
+  # Just in case some weird nesting takes place in a later schema or odd MS run type.
+  assert(len(element) == 1)
+
+  # Pick out just the scans (skip machine info, software version, etc.)
+  if '{http://sashimi.sourceforge.net/schema_revision/mzXML_3.2}scan' in element:
+    print 'win'
+  else:
+    print 'fail'
+sleep(1)
